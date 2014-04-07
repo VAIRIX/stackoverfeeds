9ae5a46bbb506b1a1ff77acdb3eaa818f117b3a2
Daniel Fernández <dfernandez@vairix.com>
2014-04-07 17:42:03 -0300

    flair was added cr: MV

--- app/helpers/stackoverfeeds/stackoverfeeds_helper.rb
+++ app/helpers/stackoverfeeds/stackoverfeeds_helper.rb
@@ -1,6 +1,6 @@
 module Stackoverfeeds
   module StackoverfeedsHelper
-    def stackoverflow_feeds(limit=ENV['stackoverflow_feeds_number'].to_i)
+    def stackoverflow_feeds(limit=ENV['feeds_number'].to_i)
       @entries = Stackoverfeeds::SoFeed.public_feeds(limit)
       render partial: 'stackoverfeeds/stackoverflow_feeds'
     end
